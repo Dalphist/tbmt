1cff5b3c351cd88b93750122ae24f8b46103c26f
DJF <399258474@qq.com>
2018-08-26 21:04:52 +0800

    shiro部分配置

---
+++ src/main/java/com/djf/tbmt/project/system/user/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.djf.tbmt.project.system.user.mapper;
+
+import com.djf.tbmt.project.system.user.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(String userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(String userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
