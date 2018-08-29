4b35c4298ce035aaa582894245a723298e88ed55
yx_zhengxl <300131@yusys.com.cn>
2018-08-29 17:59:19 +0800

    fix bugs

--- src/main/java/com/djf/tbmt/common/util/security/ShiroUtils.java
+++ src/main/java/com/djf/tbmt/common/util/security/ShiroUtils.java
@@ -1,6 +1,7 @@
 package com.djf.tbmt.common.util.security;
 
 import com.djf.tbmt.common.util.bean.BeanUtils;
+import com.djf.tbmt.framework.shiro.realm.UserRealm;
 import com.djf.tbmt.project.system.user.entity.User;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.mgt.RealmSecurityManager;
@@ -10,35 +11,29 @@ import org.apache.shiro.subject.SimplePrincipalCollection;
 import org.apache.shiro.subject.Subject;
 
 public class ShiroUtils {
-    public static Subject getSubjct()
-    {
+    public static Subject getSubjct() {
         return SecurityUtils.getSubject();
     }
 
-    public static Session getSession()
-    {
+    public static Session getSession() {
         return SecurityUtils.getSubject().getSession();
     }
 
-    public static void logout()
-    {
+    public static void logout() {
         getSubjct().logout();
     }
 
-    public static User getUser()
-    {
+    public static User getUser() {
         User user = null;
         Object obj = getSubjct().getPrincipal();
-        if (StringUtils.isNotNull(obj))
-        {
+        if (StringUtils.isNotNull(obj)) {
             user = new User();
             BeanUtils.copyBeanProp(user, obj);
         }
         return user;
     }
 
-    public static void setUser(User user)
-    {
+    public static void setUser(User user) {
         Subject subject = getSubjct();
         PrincipalCollection principalCollection = subject.getPrincipals();
         String realmName = principalCollection.getRealmNames().iterator().next();
@@ -47,30 +42,25 @@ public class ShiroUtils {
         subject.runAs(newPrincipalCollection);
     }
 
-    public static void clearCachedAuthorizationInfo()
-    {
+    public static void clearCachedAuthorizationInfo() {
         RealmSecurityManager rsm = (RealmSecurityManager) SecurityUtils.getSecurityManager();
         UserRealm realm = (UserRealm) rsm.getRealms().iterator().next();
         realm.clearCachedAuthorizationInfo();
     }
 
-    public static Long getUserId()
-    {
-        return getUser().getUserId().longValue();
+    public static String getUserId() {
+        return getUser().getUserId();
     }
 
-    public static String getLoginName()
-    {
+    public static String getLoginName() {
         return getUser().getLoginName();
     }
 
-    public static String getIp()
-    {
+    public static String getIp() {
         return getSubjct().getSession().getHost();
     }
 
-    public static String getSessionId()
-    {
+    public static String getSessionId() {
         return String.valueOf(getSubjct().getSession().getId());
     }
 }
